7c73051c102acf36c88aa133836a32d2e2aa3b06
redl-m <michael.redl14042004@gmail.com>
2025-07-17 11:24:40 +0200

    Added interactive timeline, windows messy.

--- src/app/sections/experience/experience.ts
+++ src/app/sections/experience/experience.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
-import { NgForOf, NgStyle } from '@angular/common';
-import { IntersectionObserverDirective } from './intersection-observer.directive'; // adjust path as needed
+import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
+import { IntersectionObserverDirective } from './intersection-observer.directive';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.html',
   styleUrls: ['./experience.scss'],
   standalone: true,
-  imports: [NgStyle, NgForOf, IntersectionObserverDirective]
+  imports: [NgStyle, NgForOf, IntersectionObserverDirective, NgClass, NgIf]
 })
 
 export class Experience {
   startYear = 2018;
   endYear = 2026;
 
-  visibleBars: { [key: string]: boolean } = {};
+  hoveredItem: ExperienceItem | null = null;
+
+  experiences = EXPERIENCES; // the html file can only access experiences declared in this class
 
   years: number[] = Array.from(
     { length: this.endYear - this.startYear + 2 },
@@ -31,4 +33,37 @@ export class Experience {
       width: `${widthPercent}%`
     };
   }
+
+  onHover(item: ExperienceItem | null) {
+    this.hoveredItem = item;
+  }
 }
+
+
+export interface ExperienceItem {
+  startYear: number;
+  endYear: number;
+  label: string;
+  type: 'work' | 'education';
+  info: string;
+  alignment: 'left' | 'right';
+}
+
+export const EXPERIENCES: ExperienceItem[] = [
+  {
+    startYear: 2021,
+    endYear: 2021,
+    label: 'Intern at solvistas GmbH',
+    info: 'Intern at solvistas GmbH\nTasks, tech, etc.',
+    type: 'work',
+    alignment: 'left'
+  },
+  {
+    startYear: 2018,
+    endYear: 2021.4,
+    label: 'High School',
+    info: 'High School\n2018–2021',
+    type: 'education',
+    alignment: 'left'
+  }
+];
