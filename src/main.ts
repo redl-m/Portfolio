8cee44726093f0058b419db1d20c1f711db9eaad
redl-m <michael.redl14042004@gmail.com>
2025-08-01 10:31:05 +0200

    Partial adjustments for ng build to work.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet';
 
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+// This part is fine to keep!
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'assets/marker-icon-2x.png',
+  iconUrl: 'assets/marker-icon.png',
+  shadowUrl: 'assets/marker-shadow.png',
+});
